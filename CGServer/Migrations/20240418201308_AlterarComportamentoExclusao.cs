e8d715ca766f7b6974fd2d9dcc3659fe3f03b67d
Cassio Fonseca <cassiosfonseca@gmail.com>
2024-04-18 17:47:28 -0300

    Ajuste em comportamento de banco de dados

---
+++ CGServer/Migrations/20240418201308_AlterarComportamentoExclusao.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CGServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarComportamentoExclusao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias");
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias");
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubCategorias_Categorias_CategoriaId",
+                table: "SubCategorias",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
